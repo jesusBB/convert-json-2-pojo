93f39d3a4487878c33e13bc060854f5cc45505df
Jesus Bustillo <jesus.bustillo.barrios@gmail.com>
2024-10-17 16:25:59 +0100

    tidy up

--- src/main/java/com/json2pojo/ConvertJson2PojoApplication.java
+++ src/main/java/com/json2pojo/ConvertJson2PojoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(SofascoreJsonToPojoConfiguration.class)
 public class ConvertJson2PojoApplication {
 
   public static void main(String[] args) {
